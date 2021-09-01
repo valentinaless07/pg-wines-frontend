1cbddd224f30beed91d831be9265caebfc96821e
Valentin <valentinaless07@hotmail.com>
2021-09-01 11:58:25 -0300

    barraDeNavegacion

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,6 +3,8 @@ import { NavLink, useHistory } from 'react-router-dom';
 import styles from './Navbar.module.css';
 import AuthContext from '../../auth/AuthContext';
 import { types } from '../../types/types';
+import cart from "./cart-icon.svg"
+import search from "./search.svg"
 
 const Navbar = () => {
     const { user: { name }, dispatch } = useContext(AuthContext);
@@ -18,30 +20,36 @@ const Navbar = () => {
 
     return (
         <nav className={styles.container}>
-            <ul className={styles.ulContainer}>
-                <li>
-                    <NavLink to="/">Bodegas del Sur</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/">About</NavLink>
-                </li>
-                <span>
-                    {name}
-                </span>
-                <li>
-                    <button onClick={handleLogout} >Logout</button>
-                    {/* <NavLink onClick={handleLogout} to="/login/24">Logout</NavLink> */}
-                </li>
-            </ul>
+            <div className={styles.logo_searchbar_login}>
+                <div className={styles.logo_container}>
+                    <NavLink className={styles.logo} to="/">Bodegas del Sur</NavLink>
+                </div>
+
+                <div className={styles.about_container}>
+                    <span>Sobre Nosotros</span>
+                </div>
+
+                <div className={styles.searchbar_container}>
+                    
+                    <input className={styles.searchBar} placeholder="Buscar Bebidas..." type="search"/>
+                    
+                    
+                </div>
+
+                <div className={styles.cart_login}>
+                    <span>Crear tu cuenta</span>
+                    <span>Iniciar Sesión</span>
+                    <img src={cart} alt="" />
+                    
+                </div>
+                
+            </div>
+            
+            
         </nav>
     );
 
-    // return (
-    //     <div className={styles.container}>
-    //         <h1>Navbar</h1>
-    //         user
-    //     </div>
-    // );
+   
 }
 
 export default Navbar;
