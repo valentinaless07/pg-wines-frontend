3af010c74dc33fb89cd9fd197847ab4396eab01f
benjamin-caceres <bengcaceresl26@gmail.com>
2021-09-02 08:13:45 -0600

    trabajo en footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,51 @@
 import React from 'react';
 import styles from './Footer.module.css';
+import ReactIcon from '../../Static/ReactIcon.png';
+import PostgresqlIcon from '../../Static/PostgresqlIcon.png';
+import IconRedux from '../../Static/IconRedux.png';
+import NodeIcon from '../../Static/NodeIcon.png';
+import ExpressIcon from '../../Static/ExpressIcon.png';
+
+
 
 const Footer = () => {
     return (
+        <>
         <div className={styles.container}>
-            <h1>Footer</h1>
+
+            <div className={styles.tecnologia}>
+                <div>
+                    <h2>Tecnologias usadas</h2>
+                </div>
+                <div>
+                    <img src={ReactIcon} alt="react-icon" />
+                    <img src={PostgresqlIcon} alt="postgre-icon" />
+                    <img src={IconRedux} alt="redux-icon" />
+                </div>
+                <div>
+                    <img src={NodeIcon} alt="node-icon" />
+                    <img src={ExpressIcon} alt="express-icon" />
+
+                </div>
+            </div>
+            <div className={styles.piedepagina}>
+                <div>
+                    <h2>About Bodegas del Sur</h2>
+                    <p>Bodegas del Sur es un E-commerce que nace como desafio de un grupo multinacional de estudiantes de "Henry"
+                     quienes decidieron reforzar y mostrar los conocimientos addquiridos a lo largo
+                     de un bootcamp intensivo que los introdujo de manera profesional al mundo del desarrollo web.
+                      </p>
+                </div>
+            </div>
+            <div className={styles.contactusinfooter}>
+                <button>Contactanos</button>
+            </div>
         </div>
+        <div className={styles.copyright}>
+            <p>© Copyright 2021, Todos los derechos reservados "HENRY STUDENTS"</p>
+        
+        </div>
+        </>
     )
 }
 
