28d8ac6c0252cf4278bfab72362fb3f811eaa27f
Valentin <valentinaless07@hotmail.com>
2021-09-07 22:08:14 -0300

    cart_product_delete

--- src/redux/actions/cartActions.js
+++ src/redux/actions/cartActions.js
@@ -1,8 +1,17 @@
 
 export function addCartProduct (payload) {
-    
     return {
         type: "ADD_CART_PRODUCT",
         payload
     }
 }
+
+export function deleteCartProduct(payload) {
+    
+    return {
+        type: "DELETE_CART_PRODUCT",
+        payload
+    }
+}
+
+
