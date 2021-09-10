d13578db009a62c6d6045c2a6cdbb94c6f4036dd
Diego Rodriguez <dierodz91@gmail.com>
2021-09-10 17:21:30 -0300

    Create useUser.js

---
+++ src/hooks/useUser.js
@@ -0,0 +1,55 @@
+import { useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+  getAuth,
+  signInWithPopup,
+} from "firebase/auth";
+
+import { googleAuthProvider } from "../../firebase/firebaseConfig";
+import { saveStorage } from "../../helpers/helpers";
+
+export default function useUser() {
+  const dispatch = useDispatch();
+  const { auth } = useSelector((state) => state);
+  const { replace } = useHistory();
+
+  const startGoogleLogin = useCallback(() => {
+    signInWithPopup(auth, googleAuthProvider)
+      .then(({ user }) => {
+        dispatch({
+          type: AUTH_LOGIN_SUCCESS,
+          payload: {
+            uid: user.uid,
+            displayName: user.displayName,
+            photoURL: user.photoURL,
+            email: user.email,
+          },
+        });
+        saveStorage({
+          loggedIn: false,
+          fetching: false,
+          error: null,
+          uid: user.uid,
+          displayName: user.displayName,
+          photoURL: user.photoURL,
+          email: user.email,
+        });
+        replace("/home");
+      })
+      .catch((err) => {
+        console.error(err);
+        dispatch({
+          type: AUTH_LOGIN_ERROR,
+          payload: err.message,
+        });
+      });
+  });
+
+  return {
+    startGoogleLogin,
+  };
+}
