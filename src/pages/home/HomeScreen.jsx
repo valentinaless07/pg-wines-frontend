6ba815b628ca607381073d4cdd2ef9b6e3b274c4
abadalejandro <36165616+abadalejandro@users.noreply.github.com>
2021-09-12 13:45:59 +0200

    refc: remove data origin from Slider1 component

--- src/pages/home/HomeScreen.jsx
+++ src/pages/home/HomeScreen.jsx
@@ -3,7 +3,7 @@ import Navbar from '../../components/navbar/Navbar';
 import ProductsList from '../../components/productList/ProductsList';
 import Slider1 from '../../components/slider/Slider1';
 import Footer from '../../components/footer/Footer';
-import sliderData from '../../data/slider';
+// import sliderData from '../../data/slider';
 // import styles from './HomeScreen.module.css';
 
 const HomeScreen = () => {
@@ -11,9 +11,9 @@ const HomeScreen = () => {
   return (
     <React.Fragment>
       <Navbar />
-      {/* <Slider1 data={sliderData} /> */}
+      {/* <Slider1 from={1} to={5} data={sliderData} /> */}
       &nbsp;&nbsp;&nbsp;      
-      <Slider1 data={sliderData} from={1} to={5} width={'75%'} />
+      <Slider1  width={'75%'} />
       {/* <Slider2 /> */}
       &nbsp;&nbsp;
       <ProductsList />
