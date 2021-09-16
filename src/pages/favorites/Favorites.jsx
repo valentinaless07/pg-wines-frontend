8f52b6713d81ec97c79ce96cfe78f5154f3416da
Valentín Alessandrini Zapata <79989652+valentinaless07@users.noreply.github.com>
2021-09-16 14:27:41 -0300

    mercadoPago_checkout

--- src/pages/favorites/Favorites.jsx
+++ src/pages/favorites/Favorites.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React from 'react';
+import Navbar from '../../components/navbar/Navbar';
+import styles from './Favorites.module.css';
+
 
 const Favorites = () => {
     return (
-        <div>
-            <h1>Hello from Favorites</h1>
-        </div>
+        <>
+            <Navbar />
+            <div className={styles.container}>
+                <h1>Mis Favoritos: </h1>
+            </div>
+        </>
     )
 }
-
 
 export default Favorites;
