b1b0f42d923e3a6479b4ad4a9cdf01770ae8280d
abadalejandro <36165616+abadalejandro@users.noreply.github.com>
2021-09-18 22:45:19 +0200

    feat: added dateToSpanishString, dateToString and sumToDate helper functions

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -6,32 +6,40 @@ export const saveStorage = (auth) => {
     localStorage.auth = JSON.stringify(auth);
 }
 
-export const fileUpload = async(status, file, slug, productId) => {
-                           
-    const url = 'https://pg-delsur.herokuapp.com/offers';
-    const formData = new FormData();
-    formData.append('status', status);
-    formData.append('image', file);
-    formData.append('slug', slug);
-    formData.append('productId', productId);
-    // console.log(JSON.stringify(formData));
-    
-    try{
-        const resp = await fetch(url, {
-            method: 'POST',
-            body: formData, 
-        });
-        // console.log('1:', resp);
-        if(resp.ok){
-            const result = await resp.json();
-            // console.log('2:', result);
-            return result;
-        }else{
-            throw await resp.json();
-        }
-    }catch(error){
-        console.log(error);
-        throw error;
+export const dateToSpanishString = (date) => {
+    if(typeof date === 'string'){
+        date = new Date(date);
     }
 
+    let result = '';
+    result += date.getDate();
+    if (date.getMonth() < 10) {
+        result += '/0' + (date.getMonth() + 1 + '/');
+    } else {
+        result += '/' + (date.getMonth() + 1 + '/');
+    }
+    result += date.getFullYear();
+    return result;
 }
+
+export const dateToString = (date) => {
+    if(typeof date === 'string'){
+        date = new Date(date);
+    }
+
+    let result = '';
+    result += date.getFullYear();
+    if (date.getMonth() < 10) {
+        result += '-0' + (date.getMonth() + 1);
+    } else {
+        result += '-' + (date.getMonth() + 1);
+    }
+    result += '-' + date.getDate();
+    return result;
+}
+
+export const sumToDate = (date, days) => {
+    date.setDate(date.getDate() + 1);        
+    return dateToString(date);   
+}
+
