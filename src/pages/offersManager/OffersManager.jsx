471c352b99deedfd41ad97221246180ee79c4b2c
abadalejandro <36165616+abadalejandro@users.noreply.github.com>
2021-09-21 01:30:59 +0200

    refc: remove offers manager title

--- src/pages/offersManager/OffersManager.jsx
+++ src/pages/offersManager/OffersManager.jsx
@@ -11,7 +11,7 @@ import { dateToString, sumToDate, dateToSpanishString } from '../../helpers/help
 import AdminAreaNavbar from "../../components/adminAreaNavbar/AdminAreaNavbar"
 import styles from './OffersManager.module.css';
 import './OffersManager.css';
-import AdminAreaNavbar from '../../components/adminAreaNavbar/AdminAreaNavbar';
+// import AdminAreaNavbar from '../../components/adminAreaNavbar/AdminAreaNavbar';
 
 
 const initialState = {
@@ -146,13 +146,13 @@ const OffersManager2 = ({ offersState, getOffers, postOffers, getAllProductsSlid
         <div className={styles.flex_main_container}>
             <AdminAreaNavbar />
 
-            <div className={styles.header_container}>
+            {/* <div className={styles.header_container}>
                
                 <div className={styles.title}>
                     <h1>Gestión de Ofertas</h1>
                 </div>
               
-            </div>
+            </div> */}
             <div className={styles.data_container}>
                 {
                     offersState.fetching &&
