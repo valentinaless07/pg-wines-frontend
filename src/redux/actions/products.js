7302dafee5eddf3ce7327ec3b227064bd673a109
abadalejandro <36165616+abadalejandro@users.noreply.github.com>
2021-09-20 15:06:13 +0200

    fix: removed environment variables

--- src/redux/actions/products.js
+++ src/redux/actions/products.js
@@ -5,8 +5,8 @@ export const PRODUCT_BY_NAME_RESET = 'PRODUCT_BY_NAME_RESET';
 
 export const getProductByName = (name) => {
     return async function(dispatch){
-        // await axios.get(`https://pg-delsur.herokuapp.com/products?name=${name}`)
-        await axios.get(`${process.env.REACT_APP_BACKEND_URL}/products?name=${name}`)
+        await axios.get(`https://pg-delsur.herokuapp.com/products?name=${name}`)
+        // await axios.get(`${process.env.REACT_APP_BACKEND_URL}/products?name=${name}`)
         .then (results => {
             dispatch({
                 type: GET_PRODUCT_BY_NAME,
